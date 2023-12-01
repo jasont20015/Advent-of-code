12f13989166d25d8aaf6dcc523bb95ca03842c57
jason <jason@tavernier.nl>
2023-12-01 17:01:33 +0100

    part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,33 @@
 fun main() {
+
+    /**
+     * The instruction are interpreted as follows:
+     * Each line has at least 1 numeric value
+     * The first digit and last digit of the number make up a calibration value
+     * I need to give back the sum of all calibration values.
+     *
+     * If no number is found, I just ignore it and go to the next number
+     */
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = mutableListOf<Int>()
+        for (line in input) {
+            var firstNumber = 0
+            var secondNumber = 0
+            for(c in line) {
+                if (c.isDigit()){
+                    firstNumber = c.digitToInt()*10
+                    break;
+                }
+            }
+            for(c in line.reversed()){
+                if(c.isDigit()){
+                    secondNumber = c.digitToInt()
+                    break;
+                }
+            }
+            numbers.add(firstNumber+secondNumber)
+        }
+        return numbers.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +36,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
